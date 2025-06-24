8d417fc05a0e38cc29572e3952b692c23fbf8c43
linda <linda.osman232@gmail.com>
2025-06-24 14:40:12 +0100

    Added  comments to minmax.py and age.py

--- age.py
+++ age.py
@@ -1,10 +1,14 @@
-#  calculate age in years
+#  program to calculate age in years
 
+# import date from data and time module
 from datetime import date
 
+# birthday in (yyyy,mm,dd)format
 birthday = date(1996, 2, 3)
+# get todays date
 today = date.today()
+# calculate age by subtracting year from birthday from current year
 age = today.year - birthday.year
 
-
+# display age
 print(age, "years")
--- maxmin.py
+++ maxmin.py
@@ -1,6 +1,13 @@
+# program that fins min and max value of a list
+
+# list of numbers
 numbers = [-100, 100]
+# sort list
 sorted_numbers = sorted(numbers)
+# 1st item in sorted list is the smallest
 min_val = sorted_numbers[0]
+# last item in sorted list is the largest
 max_val = sorted_numbers[-1]
+# Display age
 print("min number:", min_val)
 print("max number:", max_val)
